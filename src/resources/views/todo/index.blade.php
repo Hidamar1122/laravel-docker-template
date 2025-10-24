2d2413469aaa18ef169e5bc0738f64e10f931fdd
佐藤央典 <satouhironori@MacBook-Air.local>
2025-10-24 14:00:28 +0900

    Laravelレビュー

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -3,6 +3,7 @@
         <div class="row justify-content-center">
           <div class="col-md-8">
             <p class="text-left">
+              @dd($todos)
               <a class="btn btn-success" href="{{ route('todo.create') }}">ToDoを追加</a>
             </p>
             <div class="card">

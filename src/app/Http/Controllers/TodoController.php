07b177f2be8fc50c3436636ee756524bde5b6cc9
佐藤央典 <satouhironori@MacBook-Air.local>
2025-10-28 17:27:09 +0900

    refactor: Controllerのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,13 +7,19 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo; 
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo; 
+    }
+
+
     public function index()
     {
-      $todo = new Todo();
-      $todos = $todo->all();
       // ⭐️ $todo->all();の返り値
       // コレクションクラスのインスタンス 
-
+      $todos = $this->todo->all();
       return view('todo.index', ['todos' => $todos]);
     }
 
@@ -29,18 +35,15 @@ class TodoController extends Controller
         // ⭐️$request->all();返り値データ型
         // 連想配列
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $model = new Todo();
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 }
